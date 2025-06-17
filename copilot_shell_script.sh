caa19f0234f40e2a07e4b4b5dd76a62d8b650d9e
Eelaf-Adam <e.adam@alustudent.com>
2025-06-17 05:21:17 -0700

    title & shell permission

---
+++ copilot_shell_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# The title
+echo "============================"
+echo "Title:The Assignments upate tool"
+echo "Discription:This script file will allow the user to enter an assignment name and check it's status"
+echo "Checking your submission..."
+echo "============================"
+# Adding file permisions
+chmod +x copilot_shell_script.sh
